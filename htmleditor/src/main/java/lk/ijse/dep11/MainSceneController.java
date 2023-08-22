ca44f0e7f00dd9769e2bf6c41aff5cd9680faf2a
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2023-08-22 08:46:54 +0530

    Completed new,exit,user guide and about us

--- htmleditor/src/main/java/lk/ijse/dep11/MainSceneController.java
+++ htmleditor/src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -1,7 +1,18 @@
 package lk.ijse.dep11;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.web.HTMLEditor;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class MainSceneController {
     public MenuItem mnuOpen;
@@ -16,8 +27,17 @@ public class MainSceneController {
     public MenuItem mnuSelectAll;
     public MenuItem mnuUserGuid;
     public MenuItem mnuAboutUs;
+    public HTMLEditor htmlEditor;
+    public AnchorPane root;
 
-    public void mnuNewOnAction(ActionEvent actionEvent) {
+    public void mnuNewOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane mainSceneRoot = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+        Scene mainScene = new Scene(mainSceneRoot);
+
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(mainScene);
+        stage.setTitle("New Stage");
+        stage.show();
     }
 
     public void mnuOpenOnAction(ActionEvent actionEvent) {
@@ -34,6 +54,7 @@ public class MainSceneController {
     }
 
     public void mnuExitOnAction(ActionEvent actionEvent) {
+        System.exit(0);
     }
 
 
@@ -50,9 +71,49 @@ public class MainSceneController {
     public void mnuSelectAllOnAction(ActionEvent actionEvent) {
     }
 
-    public void mnuUserGuidOnAction(ActionEvent actionEvent) {
-    }
+    public void mnuUserGuidOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane userGuideRoot = FXMLLoader.load(getClass().getResource("/view/UserGuid.fxml"));
+        Scene scene1 = new Scene(userGuideRoot);
 
-    public void mnuAboutUsOnAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(root.getScene().getWindow());
+
+        stage.setScene(scene1);
+        stage.setTitle("User Guide");
+        stage.show();
+    }
+    double xOffset = 0;
+    double yOffset = 0;
+
+    public void mnuAboutUsOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane aboutUsRoot = FXMLLoader.load(getClass().getResource("/view/AboutUs.fxml"));
+        Scene scene1 = new Scene(aboutUsRoot);
+        Stage stage = new Stage();
+        stage.setScene(scene1);
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        aboutUsRoot.setBackground(Background.fill(Color.TRANSPARENT));
+        scene1.setFill(Color.TRANSPARENT);
+
+        //mouse drag
+
+
+        aboutUsRoot.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        // Mouse Drag Event
+        aboutUsRoot.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
+        stage.centerOnScreen();
+        stage.show();
+
+
     }
 }
